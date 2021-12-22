315cd4d548e5df1804d30021f549cb7d6a0ac98e
Ivan Yakovchuk <litvinovjobs@gmail.com>
2021-12-22 09:19:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Task1
+
+// number1 = +prompt ("Enter one number: ");
+// number2 = +prompt ("Enter two number: ");
+
+// alert ("1 = '+'\n2 = '-'\n3 = '*'\n4 = '/' " );
+// let number3 = +prompt("Enter an action: ");
+
+// switch (number3) {
+//     case 1:
+//         result = number1 + number2;
+//         break;
+//       case 2:
+//         result = number1 - number2;
+//         break;
+//       case 3:
+//         result = number1 * number2;
+//         break;
+//       case 4:
+//         result = number1 / number2;
+//         break;
+// }
+
+// alert(result);
